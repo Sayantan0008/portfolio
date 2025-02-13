ae98c5b6bc614d4dfe25fa1a88cdb40e1683aad9
Sayantan0008 <138349965+Sayantan0008@users.noreply.github.com>
2025-02-13 12:43:15 +0530

    Add files via upload

---
+++ pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import RecentProjects from "../components/RecentProjects";
+
+const Home = () => {
+  return (
+    <main>
+      <div className="home">
+        <RecentProjects />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
